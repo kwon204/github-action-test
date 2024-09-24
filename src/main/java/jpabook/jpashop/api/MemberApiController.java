fe562d09df620a33a1319e7f8bf7fdb87af937b5
kwon204 <32539398+kwon204@users.noreply.github.com>
2024-09-24 12:21:40 +0900

    Dev (#4)
    * add Make application.yml file to github actions workflows
    
    * add InitDb and MemberApiController
    
    ---------
    
    Co-authored-by: Dongkwon Kim <dafeg3@ajou.ac.kr>

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+    private final MemberService memberService;
+
+
+}
